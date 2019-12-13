f6e6395fc8a8d4b99b01c065db32028896971006
Guillermo Barreiro <gbarreiro@protonmail.com>
2019-12-13 18:20:48 +0100

    Update Gradle plugin

--- ios/Health Diary/BloodPressureViewController.swift
+++ ios/Health Diary/BloodPressureViewController.swift
@@ -15,23 +15,36 @@ class BloodPressureViewController: UIViewController {
     @IBOutlet weak var diastolicInput: UITextField!
     @IBOutlet weak var systolicAverage: UILabel!
     @IBOutlet weak var diastolicAverage: UILabel!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var randomSwitch: UISwitch!
     
     // MARK: UIActions
-    @IBAction func randomSwitchToggled(_ sender: UISwitch) {
-        // TODO: Enable or disable the input UITextFields
+    @IBAction func updateUIStatus() {
+        if(randomSwitch.isOn){
+            // With the random switch on, text inputs are always disabled and the register button is always enabled
+            systolicInput.isEnabled = false
+            diastolicInput.isEnabled = false
+            registerButton.isEnabled = true
+        
+        }else{
+            // With the random switch off, text inputs are always enabled
+            systolicInput.isEnabled = true
+            diastolicInput.isEnabled = true
+            
+            // With the random switch off, the register button will only be enabled if both text inputs aren't empty
+            registerButton.isEnabled = (!(systolicInput.text!.isEmpty) && !(diastolicInput.text!.isEmpty))
+        }
+        
     }
     
     @IBAction func registerValues() {
+        // TODO: Register values 
     }
     
     // MARK: ViewController lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    }
-    
-    @IBAction func inputTextChanged() {
-        // TODO: Enable or disable the input UITextFields
     }
     
 }
