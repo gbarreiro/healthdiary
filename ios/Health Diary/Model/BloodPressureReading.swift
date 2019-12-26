35d5b4edb454ced91b73e30343ca0c57f322cda2
Guillermo Barreiro <gbarreiro@protonmail.com>
2019-12-26 17:50:07 +0100

    Average calculations and documentation

--- ios/Health Diary/Model/BloodPressureReading.swift
+++ ios/Health Diary/Model/BloodPressureReading.swift
@@ -15,11 +15,23 @@ enum RiskLevel {
     case HYPERTENSIVE
 }
 
+/**
+    Represents a blood pressure reading, this means, the sampling of the user's systolic and diastolic blood pressure values in a specific moment. Both systolic and diastolic values are stored as Integer values, expressed in mmHg
+*/
 class BloodPressureReading {
     let systolic: Int!
     let diastolic: Int!
     let timestamp: Date!
     
+    /**
+    Blood pressure risk level, based on heart.org ranges
+    
+    # Risk levels
+    * Systolic  < 120 and Diastolic < 80: Normal
+    * Systolic < 129 and Diastolic < 80: Elevated
+    * Systolic < 180 and Diastolic < 120: High
+    * Systolic >   180 and Diastolic > 120: Hypertensive
+    */
     var riskLevel: RiskLevel{
         if (systolic < 120 && diastolic < 80) {return .NORMAL}
         if (systolic < 129 && diastolic < 80) {return .ELEVATED}
@@ -27,6 +39,9 @@ class BloodPressureReading {
         else {return .HYPERTENSIVE}
     }
     
+    /**
+        Random constructor. Creates a blood pressure reading with random values.
+     */
     init(timestamp: Date){
         // Random diastolic and systolic values
         self.timestamp = timestamp
@@ -34,12 +49,32 @@ class BloodPressureReading {
         self.diastolic = Int.random(in: 60...105)
     }
     
+    /**
+        Creates a blood pressure  measure with the specififed systolic and diastolic values.
+     */
     init(systolic: Int, diastolic: Int, timestamp: Date){
         self.systolic = systolic
         self.diastolic = diastolic
         self.timestamp = timestamp
     }
     
-    
+    /**
+        Creates a record with the average values from a list of readings
+        - Parameter records:  list with BloodPressureReading records, whose average values you desire to calculate
+     */
+    init(records: [BloodPressureReading]) {
+        var systolic = 0, diastolic = 0
+        for record in records{
+            systolic += record.systolic
+            diastolic += record.diastolic
+        }
+        systolic /= records.count
+        diastolic /= records.count
+        
+        self.systolic = systolic
+        self.diastolic = diastolic
+        self.timestamp = Date() // date is not relevant for an average object
+        
+    }
     
 }
--- ios/Health Diary/Model/BodyMeasureReading.swift
+++ ios/Health Diary/Model/BodyMeasureReading.swift
@@ -15,10 +15,24 @@ enum BMILevel {
     case OBESE
 }
 
+/**
+    Represents a body measure reading, this means, the sampling of the user's weight and height in a specific moment. The weight is stored as a decimal number in kg, and the height as an integer in cm.
+*/
 class BodyMeasureReading {
     let weight: Float!
     let height: Int!
     let timestamp: Date!
+    
+    /**
+     Body measure index is calculated as kg / (m)².
+     This index gives a ratio of how low or high is the weight of a person.
+     
+     # Risk levels
+     * <18.5: Underweight
+     * 18.5 - 24.9: healthy
+     * 25 - 29.9: overweight
+     * 30 - 39.9: obese
+     */
     lazy var bmi = weight / pow((Float(height)/100), 2)
     
     var bmiLevel: BMILevel {
@@ -28,15 +42,40 @@ class BodyMeasureReading {
         else { return .OBESE }
     }
     
+    /**
+        Random constructor. Creates a body measure reading with random weight and height.
+     */
     init(timestamp: Date){
         self.timestamp = timestamp
         self.height = Int.random(in: 120...200)
         self.weight = Float.random(in: 40...100)
     }
     
+    /**
+        Creates a body measure with the specififed weight and height.
+     */
     init(weight: Float, height: Int, timestamp: Date){
         self.timestamp = timestamp
         self.weight = weight
         self.height = height
+    }
+    
+    /**
+        Creates a record with the average values from a list of readings
+        - Parameter records:  list with BodyMeasure records, whose average values you desire to calculate
+     */
+    init(records: [BodyMeasureReading]) {
+        var weight:Float = 0.0, height = 0
+        for record in records{
+            weight += record.weight
+            height += record.height
+        }
+        weight /= Float(records.count)
+        height /= records.count
+        
+        self.weight = weight
+        self.height = height
+        self.timestamp = Date() // date is not relevant for an average object
+        
     }
 }
